78dfdf06f044691fc87a2ced5246a2fd4f086a98
twelve0 <twelve.abdelali@gmail.com>
2023-01-03 12:17:28 +0100

    init

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	app.useGlobalPipes(new ValidationPipe());
+	app.enableCors({
+		origin: true,
+	});
+
+	await app.listen(3000);
 }
 bootstrap();
